f564419ad7e4389875c9bb015321ef2cda20d79e
Awalfajri001 <116197604+Awalfajri001@users.noreply.github.com>
2022-11-06 22:31:45 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // c = b+a;
+            int b;
+            int a;
+            int c;
+            Console.WriteLine("Masukkan Nilai b =");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Masukkan Nilai a=");
+            a = int.Parse(Console.ReadLine());
+
+            c= b+a;
+
+            Console.WriteLine("Masukkan Hasil c adalah "+c);
+        }
+    }
+}
